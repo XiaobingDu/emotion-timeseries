2fdc6b92c014430682ddd21249c00d39be56f13e
XiaobingDu <1551972411@qq.com>
2021-03-25 16:27:42 +0800

    update

--- emotion-timeseries/positiveEmotion/model_co_attn_GC.py
+++ emotion-timeseries/positiveEmotion/model_co_attn_GC.py
@@ -298,7 +298,8 @@ class MovieNet(nn.Module):
             # softmax layer
             # log_softmax = torch.nn.LogSoftmax(dim=1)
             # log_softmax_output = log_softmax(emot_score)
-            predicted = torch.nn.Softmax(predicted_last, dim=1)
+            softmax = torch.nn.Softmax(dim=1)
+            predicted = softmax(predicted_last)
         else:
             # Use earlier predictions to predict next time-steps
             predicted = []
