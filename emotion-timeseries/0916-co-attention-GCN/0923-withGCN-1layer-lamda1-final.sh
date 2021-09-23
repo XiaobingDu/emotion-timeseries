c4d75ac98b1e4790652d054df85dd3285b1654cf
XiaobingDu <1551972411@qq.com>
2021-09-23 23:33:42 +0800

    update

--- emotion-timeseries/0916-co-attention-GCN/0923-withGCN-1layer-lamda1-final.sh
+++ emotion-timeseries/0916-co-attention-GCN/0923-withGCN-1layer-lamda1-final.sh
@@ -27,7 +27,7 @@
 #
 #CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 32 --overlap without --save_file 0409_win20_WOO_multilabelMetrics_FF_withdecoder_withoutGCN --log_dir 0406_five_fold --lamda 0.9 --fold_id 1 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 
-CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 1 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
+CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_output256_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 1 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 
 
 
@@ -49,7 +49,7 @@ CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_siz
 #
 #CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 32 --overlap without --save_file 0409_win20_WOO_multilabelMetrics_FF_withdecoder_withoutGCN --log_dir 0406_five_fold --lamda 0.9 --fold_id 2 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 #
-CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 2 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
+CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_output256_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 2 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 
 
 
@@ -71,7 +71,7 @@ CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_siz
 #
 #CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 32 --overlap without --save_file 0409_win20_WOO_multilabelMetrics_FF_withdecoder_withoutGCN --log_dir 0406_five_fold --lamda 0.9 --fold_id 3 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 #
-CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 3 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
+CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_output256_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 3 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 
 
 
@@ -93,7 +93,7 @@ CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_siz
 #
 #CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 32 --overlap without --save_file 0409_win20_WOO_multilabelMetrics_FF_withdecoder_withoutGCN --log_dir 0406_five_fold --lamda 0.9 --fold_id 4 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 #
-CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 4 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
+CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_output256_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 4 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 
 
 
@@ -115,4 +115,4 @@ CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_siz
 #
 #CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 32 --overlap without --save_file 0409_win20_WOO_multilabelMetrics_FF_withdecoder_withoutGCN --log_dir 0406_five_fold --lamda 0.9 --fold_id 5 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
 #
-CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 5 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
+CUDA_VISIBLE_DEVICES=1 python train_co_attn.py  --strategy five_fold --batch_size 64 --LSTM_hidden 1024 --overlap with --save_file 0922_final_withGCN_1layer_output256_lamda1_win120_over30 --log_dir 0916_five_fold --lamda 1.0 --fold_id 5 --path1 ../EEG_PSD_9_DOM/   --path2 ../DOM_feature_all/DOM_featureAll.mat
