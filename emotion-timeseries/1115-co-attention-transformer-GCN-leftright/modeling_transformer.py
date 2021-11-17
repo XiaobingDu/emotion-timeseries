10f40e42a6ac51e0da938ddb9c7f27652877ba4f
XiaobingDu <1551972411@qq.com>
2021-11-17 22:39:01 +0800

    update

--- emotion-timeseries/1115-co-attention-transformer-GCN-leftright/modeling_transformer.py
+++ emotion-timeseries/1115-co-attention-transformer-GCN-leftright/modeling_transformer.py
@@ -19,7 +19,7 @@ class MultiSequential(nn.Sequential):
             if type(inputs) == tuple:
                 inputs = module(*inputs)
             else:
-                inputs = module(inputs)
+                inputs = module(inputs.cuda())
         return inputs
 
 
@@ -117,7 +117,7 @@ class MultiHeadAttention(nn.Module):
             )
             mask = mask.reshape(-1).repeat((1, np.prod(score.shape[:2]))).reshape(score.shape)
 
-            score[mask.type(torch.long)] = -float('inf')
+            score[mask.type(torch.long).cuda()] = -float('inf')
 
         self.att = F.softmax(score / np.sqrt(score.shape[-1]), dim=-1)
         ret = torch.einsum('bhqk,bkhd->bqhd', self.att, value)
