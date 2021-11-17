e4e0bb9ab53d23765b6393a490c555f348b6c229
XiaobingDu <1551972411@qq.com>
2021-11-17 23:50:06 +0800

    update

--- emotion-timeseries/1115-co-attention-transformer-GCN-leftright/model.py
+++ emotion-timeseries/1115-co-attention-transformer-GCN-leftright/model.py
@@ -54,7 +54,7 @@ class EEGEncoder(nn.Module):
 
         self.left_transformer_enc = TransformerEncoder(self.left_len, self.feature_dim, self.hidden_dim,  nheads=5, depth=2, p=0.1, max_len=600)
         self.right_transformer_enc = TransformerEncoder(self.right_len, self.feature_dim,self.hidden_dim, nheads=5, depth=2, p=0.1, max_len=600)
-        self.all_transformer_enc = TransformerEncoder(self.sequence_len, self.feature_len,self.hidden_dim, nheads=5, depth=2, p=0.1, max_len=600)
+        self.all_transformer_enc = TransformerEncoder(30, 600,self.hidden_dim, nheads=5, depth=2, p=0.1, max_len=600)
 
         # [left right]-->att_linear
         self.att_linear = nn.Sequential(nn.Dropout(self.dropout),nn.Linear(self.hidden_dim * 2, 1), nn.LeakyReLU())
@@ -106,15 +106,15 @@ class EEGEncoder(nn.Module):
         att_score = torch.softmax(att_score, dim=-1)
 
         # all_transformer
-        left_features = torch.reshape(left_features,[left_features.shape[0],left_features.shape[1], 120, 5])
-        left_features = left_features.permute(0,2,1,3)
-        left_features = torch.reshape(left_features,[left_features.shape[0],left_features.shape[1],left_features.shape[2]*left_features.shape[3]])
+        # left_features = torch.reshape(left_features,[left_features.shape[0],left_features.shape[1], 120, 5])
+        # left_features = left_features.permute(0,2,1,3)
+        # left_features = torch.reshape(left_features,[left_features.shape[0],left_features.shape[1],left_features.shape[2]*left_features.shape[3]])
         print('left_feature shape:', left_features.shape)
-        right_features = torch.reshape(right_features, [right_features.shape[0], right_features.shape[1], 120, 5])
-        right_features = right_features.permute(0, 2, 1, 3)
-        right_features = torch.reshape(right_features, [right_features.shape[0], right_features.shape[1], right_features.shape[2]*right_features.shape[3]])
+        # right_features = torch.reshape(right_features, [right_features.shape[0], right_features.shape[1], 120, 5])
+        # right_features = right_features.permute(0, 2, 1, 3)
+        # right_features = torch.reshape(right_features, [right_features.shape[0], right_features.shape[1], right_features.shape[2]*right_features.shape[3]])
         print('right_feature shape:', right_features.shape)
-        all_features = torch.cat([left_features, right_features], dim=-1).cuda()
+        all_features = torch.cat([left_features, right_features], dim=1).cuda()
         print('all_feature shape:', all_features.shape)
         presentation = self.all_transformer_enc(all_features)
         print('presentation shape:', presentation.shape)
