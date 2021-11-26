c0da7d210676088377d14809ef587f6ed7509d55
XiaobingDu <1551972411@qq.com>
2021-11-26 18:28:42 +0800

    update

--- emotion-timeseries/1115-co-attention-transformer-GCN-leftright/utils.py
+++ emotion-timeseries/1115-co-attention-transformer-GCN-leftright/utils.py
@@ -263,8 +263,8 @@ def gen_A(num_classes, t, p, adj_file):
     _adj = _adj / _nums
     _adj[_adj < t] = 0
     _adj[_adj >= t] = 1
-    _adj = _adj + np.identity(num_classes, np.int)
     _adj = _adj * p / (_adj.sum(0, keepdims=True) + 1e-6) #p=0.25
+    _adj = _adj + np.identity(num_classes, np.int)*(1-p)
     print(_adj.sum(0, keepdims=True))
     print('^^^^^^', _adj)
     return _adj
