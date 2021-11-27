d0b2c32de0457a16003263aae56bb7739846a9fd
XiaobingDu <1551972411@qq.com>
2021-11-27 16:55:34 +0800

    update

--- emotion-timeseries/1123-co-attention-transformer-WOGCN-leftright/multilabelMetrics/labelbasedclassification.py
+++ emotion-timeseries/1123-co-attention-transformer-WOGCN-leftright/multilabelMetrics/labelbasedclassification.py
@@ -18,9 +18,9 @@ def accuracyMacro(y_test, predictions):
     y_test = y_test.cpu().detach().numpy()
     print(y_test)
     predictions = predictions.cpu().detach().numpy()
-    print('####', predictions)
+    # print('####', predictions)
     predict_label = np.array(predictions > 0.500, dtype=float)
-    print('****', predict_label)
+    # print('****', predict_label)
 
     accuracymacro = 0.0
     TP, FP, TN, FN = multilabelConfussionMatrix(y_test, predict_label)
