cc884f9b5b50a713ba8bf320450b626587bad71a
XiaobingDu <1551972411@qq.com>
2021-11-29 22:25:07 +0800

    update

--- emotion-timeseries/1130-transformer-timestep-labelCorr-All/modeling_transformer.py
+++ emotion-timeseries/1130-transformer-timestep-labelCorr-All/modeling_transformer.py
@@ -128,7 +128,7 @@ class MultiHeadAttention(nn.Module):
             score = torch.mul(score, mask)
 
         self.att = F.softmax(score / np.sqrt(score.shape[-1]), dim=-1)
-        ret = torch.einsum('bhqk,bkhd->bqhd', self.att, value.double())
+        ret = torch.einsum('bhqk,bkhd->bqhd', self.att.float(), value)
 
         return ret
 
