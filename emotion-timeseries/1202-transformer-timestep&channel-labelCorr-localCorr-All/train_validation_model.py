f495c0cb58ffa4576139896457a9ae545c025974
XiaobingDu <1551972411@qq.com>
2021-12-25 18:49:47 +0800

    update

--- emotion-timeseries/1202-transformer-timestep&channel-labelCorr-localCorr-All/train_validation_model.py
+++ emotion-timeseries/1202-transformer-timestep&channel-labelCorr-localCorr-All/train_validation_model.py
@@ -539,8 +539,10 @@ for epoch_num in range(num_epochs):
         sum_accuracyMacro += val_accuracyMacro
         val_per_A = torch.tensor(val_per_A)
         print('----',len(val_per_A))
-        sum_per_A += val_per_A
+        print(val_per_A)
         print('----', len(sum_per_A))
+        print(sum_per_A)
+        sum_per_A += val_per_A
         val_accuracyMicro = accuracyMicro(dom_label, predict_sig)
         sum_accuracyMicro += val_accuracyMicro
         val_precisionMacro, val_per_P = precisionMacro(dom_label, predict_sig)
