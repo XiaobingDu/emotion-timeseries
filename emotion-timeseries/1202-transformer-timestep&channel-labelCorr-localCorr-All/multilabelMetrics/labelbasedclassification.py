1b75a7b98c3145d9a6b2da255e7542d407c4d6e1
XiaobingDu <1551972411@qq.com>
2021-12-25 19:23:26 +0800

    update

--- emotion-timeseries/1202-transformer-timestep&channel-labelCorr-localCorr-All/multilabelMetrics/labelbasedclassification.py
+++ emotion-timeseries/1202-transformer-timestep&channel-labelCorr-localCorr-All/multilabelMetrics/labelbasedclassification.py
@@ -36,7 +36,7 @@ def accuracyMacro(y_test, predictions):
 
     accuracymacro = float(accuracymacro / len(TP))
 
-    print('******', per_accuracy)
+    # print('******', per_accuracy)
     return accuracymacro, per_accuracy
 
 
@@ -219,7 +219,7 @@ def fbetaMacro(y_test, predictions, beta=1):
             per_f1.append(num / den)
 
     fbetamacro = fbetamacro / len(TP)
-    print('******', per_f1)
+    # print('******', per_f1)
     return fbetamacro, per_f1
 
 
