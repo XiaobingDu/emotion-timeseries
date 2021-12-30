e4e6962c3497b7c3891c68ee61ecd16039410c51
XiaobingDu <1551972411@qq.com>
2021-12-30 11:14:17 +0800

    update

--- emotion-timeseries/1202-transformer-timestep&channel-labelCorr-localCorr-All/train_validation_model.py
+++ emotion-timeseries/1202-transformer-timestep&channel-labelCorr-localCorr-All/train_validation_model.py
@@ -664,7 +664,7 @@ for epoch_num in range(num_epochs):
             # result.write('att_1: \t')
             # result.write('%s\n' % att_1.cpu().detach().numpy().mean(axis=0))
         print('Validation Attention for Every Instance:\n ')
-        print('%s\n' % dom_label.cpu().detach().numpy())
+        print('%s\n' % attention.cpu().detach().numpy().mean(axis=0))
         result.write("\n*****************************************************************************\n")
         result.write('Validation Attention for Every Instance:\n ')
         result.write('%s\n' % dom_label.cpu().detach().numpy())
